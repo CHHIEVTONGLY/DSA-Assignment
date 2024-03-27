39f5af833046431110f83829e5bfeb0f26a4ada7
LEVI2612 <103377836+CHHIEVTONGLY@users.noreply.github.com>
2024-03-27 13:59:58 +0700

    Create User
    Create user and then after input it into it make it create a text file to store the data

--- Bank-management/Bank.c
+++ Bank-management/Bank.c
@@ -1,31 +1,88 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main() {
-    int option ;
+typedef struct{
+	char username[20];
+	char email[40];
+	char phoneNumber[12];
+	char password[20];
+	
+} Data;
+    Data inputData(void){
+	Data items;
+	printf("Input username     :");
+    fgets(items.username, sizeof(items.username), stdin);
+	printf("Input email        :");
+    fgets(items.email, sizeof(items.email), stdin);
+	printf("Input phone number :");
+    fgets(items.phoneNumber, sizeof(items.phoneNumber), stdin);
+	printf("Input password     :");
+    fgets(items.password, sizeof(items.password), stdin);
+	return items;
+}
+void output(Data items){
+	printf("username    :%s\n",items.username);
+	printf("email       :%s\n",items.email);
+	printf("phone number:%s\n",items.phoneNumber);
+	// printf("password    :%s\n",items.password);
+}
+int main(){
+	int option;
+	FILE *account_data;
+	int n,i;
+	do{
+	    printf("\n\n\n");
+	    printf("\033[0;36m \t ########     ###    ##    ## ##    ##    ##     ##    ###    ##    ##    ###     ######   ######## ##     ## ######## ##    ## ######## \033[0m\n");
+	    printf("\033[0;36m \t ##     ##   ## ##   ###   ## ##   ##     ###   ###   ## ##   ###   ##   ## ##   ##    ##  ##       ###   ### ##       ###   ##    ##    \033[0m\n");
+	    printf("\033[0;36m \t ##     ##  ##   ##  ####  ## ##  ##      #### ####  ##   ##  ####  ##  ##   ##  ##        ##       #### #### ##       ####  ##    ##    \033[0m\n");
+	    printf("\033[0;36m \t ########  ##     ## ## ## ## #####       ## ### ## ##     ## ## ## ## ##     ## ##   #### ######   ## ### ## ######   ## ## ##    ##    \033[0m\n");
+	    printf("\033[0;36m \t ##     ## ######### ##  #### ##  ##      ##     ## ######### ##  #### ######### ##    ##  ##       ##     ## ##       ##  ####    ##    \033[0m\n");
+	    printf("\033[0;36m \t ##     ## ##     ## ##   ### ##   ##     ##     ## ##     ## ##   ### ##     ## ##    ##  ##       ##     ## ##       ##   ###    ##    \033[0m\n");
+	    printf("\033[0;36m \t ########  ##     ## ##    ## ##    ##    ##     ## ##     ## ##    ## ##     ##  ######   ######## ##     ## ######## ##    ##    ##    \033[0m\n");
+	    printf("\n\n\n");
+	    printf("1. Create Acoount \n");
+	    printf("2. Login Account\n");
+	    printf("Option [0] to exits : ");
+	    scanf("%d" , &option); 
 
-    printf("\n\n\n");
-    printf("\033[0;36m \t ########     ###    ##    ## ##    ##    ##     ##    ###    ##    ##    ###     ######   ######## ##     ## ######## ##    ## ######## \033[0m\n");
-    printf("\033[0;36m \t ##     ##   ## ##   ###   ## ##   ##     ###   ###   ## ##   ###   ##   ## ##   ##    ##  ##       ###   ### ##       ###   ##    ##    \033[0m\n");
-    printf("\033[0;36m \t ##     ##  ##   ##  ####  ## ##  ##      #### ####  ##   ##  ####  ##  ##   ##  ##        ##       #### #### ##       ####  ##    ##    \033[0m\n");
-    printf("\033[0;36m \t ########  ##     ## ## ## ## #####       ## ### ## ##     ## ## ## ## ##     ## ##   #### ######   ## ### ## ######   ## ## ##    ##    \033[0m\n");
-    printf("\033[0;36m \t ##     ## ######### ##  #### ##  ##      ##     ## ######### ##  #### ######### ##    ##  ##       ##     ## ##       ##  ####    ##    \033[0m\n");
-    printf("\033[0;36m \t ##     ## ##     ## ##   ### ##   ##     ##     ## ##     ## ##   ### ##     ## ##    ##  ##       ##     ## ##       ##   ###    ##    \033[0m\n");
-    printf("\033[0;36m \t ########  ##     ## ##    ## ##    ##    ##     ## ##     ## ##    ## ##     ##  ######   ######## ##     ## ######## ##    ##    ##    \033[0m\n");
-    printf("\n\n\n");
+        while (getchar() != '\n'); //Clear input buffer
 
-    printf("1. Create Acoount \n");
-    printf("2. Deposit \n");
-    printf("3. Withdraw \n");
-    printf("4. Search ID \n\n");
-
-    printf("Option : ");
-    scanf("%d" , &option); 
-
-    printf("You choose : %d " , option);
-
-    printf("Testing");
-
-    
-    
-    return 0;
+    	switch(option){
+    		case 1:{
+			   	printf("Input user :");
+                scanf("%d",&n);
+                
+                while (getchar() != '\n');  // Clear input buffer
+                
+				account_data=fopen("account_data.txt","w");
+				Data user[20];
+				if(account_data==NULL){
+					printf("Error!");
+					return 0;
+				}
+				for(i=0;i<n;i++){
+					printf("========>User[%d]<===========\n",i+1);
+					user[i]=inputData();
+				}
+			
+				for (i = 0; i < n; i++) {
+			        fwrite(&user[i], sizeof(Data), 1, account_data); // Write each object to file
+			    }
+				fclose(account_data);
+				account_data=fopen("account_data.txt","r");
+                
+				Data temp;
+			    while (fread(&temp, sizeof(temp), 1, account_data)) {
+			        output(temp);
+			    }
+			
+			}break;
+			case 2:{
+				
+				
+			}break;
+		}
+	}while(option != 0);
+	return 0;
 }
